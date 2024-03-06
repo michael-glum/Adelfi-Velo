ec024bd1bfe49fb7d3a555310f74c5fb93dcbec1
michael-glum <61207272+michael-glum@users.noreply.github.com>
2024-03-06 18:59:13 -0500

    Update sororitySelection.js

--- Frontend/sororitySelection.js
+++ Frontend/sororitySelection.js
@@ -1,7 +1,7 @@
 // Velo API Reference: https://www.wix.com/velo/reference/api-overview/introduction
 import { local, session } from 'wix-storage-frontend';
 import wixLocation from 'wix-location';
-import wixData from 'wix-data';
+import { checkChapterExists } from 'backend/sororities-server.web';
 
 $w.onReady(function () {
 
@@ -22,20 +22,19 @@ $w.onReady(function () {
 
 	submitButton.onClick(() => {
 		let selectedChapter = dropdownList.value;
-		wixData.query('Sororities')
-			.eq('chapter', selectedChapter)
-			.find()
+
+		checkChapterExists(selectedChapter)
 			.then(results => {
-				if(results.items.length === 0) {
-					// If the selected chapter is not found in the database, highlight the selection in red to show it is incorrect.
+				if (results.length === 0) {
+					// Selected chapter does not exist. Show error.
 					dropdownList.value = "";
 					dropdownList.onCustomValidation((value, reject) => {
-  						if(value === "") {
-    						reject("Chapter not found."); // Should show built in error box, but does not. 
-  						}
+						if (value === "") {
+							reject("Chapter not found.");
+						}
 					});
 				} else {
-					//submitButton.disable(); // (Low priority) Does not work as expected. Button should grey out after it is clicked. Possibly overriden by an End Hover event.
+					// Selected chapter exists. Store it in local & session storage, then redirect to shopping page.
 					session.setItem('selectedChapter', selectedChapter);
 					local.setItem('selectedChapter', selectedChapter);
 					wixLocation.to('/shopping');
